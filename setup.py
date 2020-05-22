7a5f41340f5edf913e07ef023c1570bc84fc6c50
sudhanshu mathur <sudhanshumathur01>
2020-05-22 16:51:03 +0530

    second commint

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ REQUIRES_PYTHON = '>=3.7.0'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
+REQUIRED = ["click"
 ]
+
 
 # What packages are optional?
 EXTRAS = {
@@ -107,9 +107,9 @@ setup(
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
 
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
+     entry_points={
+         'console_scripts': ['assault=assault.cli:cli'],
+     },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
